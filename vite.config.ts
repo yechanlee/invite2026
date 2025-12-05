fa07568abbca8b09d62c6551aab3c33b8732dde4
yechanlee <firecare@naver.com>
2025-12-05 22:46:28 +0900

    nojykell

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,17 @@
-import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
-  const base = env.VITE_BASE_PATH || '/'
+  const env = loadEnv(mode, process.cwd(), "");
+  const base = env.VITE_BASE_PATH || "/";
 
   return {
     plugins: [react()],
     base,
-  }
-})
+    build: {
+      outDir: "docs",
+      assetsDir: "assets",
+    },
+  };
+});
